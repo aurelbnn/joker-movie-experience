67e657aa7228a67d483a66777d8bba3ff3fc957e
aurelbnn <49645033+aurelbnn@users.noreply.github.com>
2019-10-09 16:53:16 +0200

    add legal part

--- scripts/casting.js
+++ scripts/casting.js
@@ -1,11 +1,4 @@
 const container = document.querySelector('.container'),
-      playButton1 = document.querySelector('#playButton1'),
-      playButton2 = document.querySelector('#playButton2'),
-      playButton3 = document.querySelector('#playButton3'),
-      playButton4 = document.querySelector('#playButton4'),
-      playButton5 = document.querySelector('#playButton5'),
-      playButton6 = document.querySelector('#playButton6'),
-
       firstCard = document.querySelector('#firstCard')
       secondCard = document.querySelector('#secondCard')
       thirdCard = document.querySelector('#thirdCard')
@@ -56,46 +49,26 @@ firstCard.addEventListener('mouseover', function() {
   firstCardAnimation.play();
 });
 
-firstCard.addEventListener('click', function() {
-  firstCardZoom.play();
-});
-
 secondCard.addEventListener('mouseover', function() {
   secondCardAnimation.play();
 });
 
-secondCard.addEventListener('click', function() {
-  secondCardZoom.play();
-});
 
 thirdCard.addEventListener('mouseover', function() {
   thirdCardAnimation.play();
 });
 
-thirdCard.addEventListener('click', function() {
-  thirdCardZoom.play();
-});
 
 fourthCard.addEventListener('mouseover', function() {
   fourthCardAnimation.play();
 });
 
-fourthCard.addEventListener('click', function() {
-  fourthCardZoom.play();
-});
 
 fifthCard.addEventListener('mouseover', function() {
   fifthCardAnimation.play();
 });
 
-fifthCard.addEventListener('click', function() {
-  fifthCardZoom.play();
-});
 
 sixthCard.addEventListener('mouseover', function() {
   sixthCardAnimation.play();
-});
-
-sixthCard.addEventListener('click', function() {
-  sixthCardZoom.play();
 });
