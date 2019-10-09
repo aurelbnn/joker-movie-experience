b8a4837dba74703a1f6f957475538533375aacf6
aurelbnn <49645033+aurelbnn@users.noreply.github.com>
2019-10-09 15:24:16 +0200

    rotation animation in casting page

--- scripts/casting.js
+++ scripts/casting.js
@@ -1,5 +1,11 @@
 const container = document.querySelector('.container'),
-      playButton = document.querySelector('.playButton'),
+      playButton1 = document.querySelector('#playButton1'),
+      playButton2 = document.querySelector('#playButton2'),
+      playButton3 = document.querySelector('#playButton3'),
+      playButton4 = document.querySelector('#playButton4'),
+      playButton5 = document.querySelector('#playButton5'),
+      playButton6 = document.querySelector('#playButton6'),
+
       firstCard = document.querySelector('#firstCard')
       secondCard = document.querySelector('#secondCard')
       thirdCard = document.querySelector('#thirdCard')
@@ -10,41 +16,85 @@ const container = document.querySelector('.container'),
 TweenMax.set(container, {perspective:1000});
 
 const firstCardAnimation = TweenMax.to(firstCard, 2, {
-  rotationY: -360,
-  x:850,
+  rotationY: 360,
   paused: true,
 });
 
 const secondCardAnimation = TweenMax.to(secondCard, 2, {
-  rotationY: -180,
-  x:550,
+  rotationY: 360,
   paused: true,
 });
 
 const thirdCardAnimation = TweenMax.to(thirdCard, 2, {
-  rotationY: -180,
-  x:850,
+  rotationY: 360,
   paused: true,
 });
 
 const fourthCardAnimation = TweenMax.to(fourthCard, 2, {
-  rotationY: -180,
-  x:1150,
+  rotationY: 360,
   paused: true,
 });
 
 const fifthCardAnimation = TweenMax.to(fifthCard, 2, {
-  rotationY: -180,
-  x:1150,
+  rotationY: 360,
   paused: true,
 });
 
 const sixthCardAnimation = TweenMax.to(sixthCard, 2, {
-  rotationY: -180,
-  x:1150,
+  rotationY: 360,
   paused: true,
 });
 
-playButton.addEventListener('click', function() {
+const firstCardZoom = document.createElement("div");
+
+// CARD ANIMATION AND ZOOM TO SEE COMPLETED INFORMATIONS
+// ANIMATION WITH MOUSEOVER TO ROTATE A CARD
+// ANIMATION WITH THE CLICK TO ZOOM AND SHOW THE INFORMATIONS
+
+firstCard.addEventListener('mouseover', function() {
   firstCardAnimation.play();
+});
+
+firstCard.addEventListener('click', function() {
+  firstCardZoom.play();
+});
+
+secondCard.addEventListener('mouseover', function() {
+  secondCardAnimation.play();
+});
+
+secondCard.addEventListener('click', function() {
+  secondCardZoom.play();
+});
+
+thirdCard.addEventListener('mouseover', function() {
+  thirdCardAnimation.play();
+});
+
+thirdCard.addEventListener('click', function() {
+  thirdCardZoom.play();
+});
+
+fourthCard.addEventListener('mouseover', function() {
+  fourthCardAnimation.play();
+});
+
+fourthCard.addEventListener('click', function() {
+  fourthCardZoom.play();
+});
+
+fifthCard.addEventListener('mouseover', function() {
+  fifthCardAnimation.play();
+});
+
+fifthCard.addEventListener('click', function() {
+  fifthCardZoom.play();
+});
+
+sixthCard.addEventListener('mouseover', function() {
+  sixthCardAnimation.play();
+});
+
+sixthCard.addEventListener('click', function() {
+  sixthCardZoom.play();
 });
