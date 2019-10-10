2e739d0d7e80e2c1169e93d445b20678411fee10
aurelbnn <49645033+aurelbnn@users.noreply.github.com>
2019-10-10 10:48:46 +0200

    test

--- scripts/casting.js
+++ scripts/casting.js
@@ -1,6 +1,28 @@
 const container = document.querySelector('.container'),
-      secondCard = document.querySelector('#secondCard')
-      thirdCard = document.querySelector('#thirdCard')
-      fourthCard = document.querySelector('#fourthCard')
-      fifthCard = document.querySelector('#fifthCard')
-      sixthCard = document.querySelector('#sixthCard')
+      test = document.querySelector('.test'),
+      firstCastingCard = document.querySelector('#joaquin_phoenix')
+      zoomedFirstCard = document.querySelector('#zoomed_joaquin_phoenix')
+      zoomedSecondCard = document.querySelector('#zoomed_robert_deniro')
+      zoomedThirdCard = document.querySelector('#zoomed_shea_whigham')
+      zoomedFourthCard = document.querySelector('#zoomed_frances_conroy')
+      zoomedFifthCard = document.querySelector('#zoomed_zazie_beetze')
+      zoomedSixthCard = document.querySelector('#zoomed_brett_cullen')
+
+
+
+const zoomedFirstCardAnimation = TweenMax.to(firstCard, 2, {
+  visibility: visible;
+
+const secondCardAnimation = TweenMax.to(secondCard, 2, {
+  rotationY: 360,
+  paused: true,
+});
+
+fonction testingit() {
+  
+}
+
+
+test.addEventListener('click', function() {
+
+});
