4da5962129ac0a44c3c18599f17ad854f0f66bdc
aurelbnn <49645033+aurelbnn@users.noreply.github.com>
2019-10-11 14:24:07 +0200

    backgroud size

--- scripts/experience.js
+++ scripts/experience.js
@@ -1,7 +1,8 @@
-let body = document.querySelector("body")
+let body = document.querySelector("body"),
 
-let opacityChange = $('.jokerApparition');
+
+let opacityChange = document.querySelector('.jokerApparition');
 $(window).on('scroll', function() {
    let st = $(this).scrollTop();
-   opacityChange.css({ 'opacity' : (0 + st/35) });
+   opacityChange.css({ 'opacity' : (0 - st/35) });
 });
